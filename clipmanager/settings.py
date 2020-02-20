14ecac1569ead643d0441ef196cab141fd7178ad
JackFltnn <2422024617@qq.com>
2020-02-20 14:04:32 +0800

    Update settings.py

--- clipmanager/settings.py
+++ clipmanager/settings.py
@@ -23,7 +23,7 @@ MEDIA_URL = "/upload/"
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zhlpzynrzw=qa=+p*ft9$_iqm+7d3a+q@$2=v07+8t&!twg(72'
+SECRET_KEY = '233'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
